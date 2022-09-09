ff2c82d45f9d098c1c35647e03539efc53cd1df9
Sbastdia <sbastdia@myuax.com>
2022-09-09 19:00:08 +0200

    separando clases

---
+++ automoviles/camioneta.py
@@ -0,0 +1,8 @@
+from coche import Coche
+
+class Camioneta(Coche):
+    def __init__(self, color, ruedas, velocidad, cilindrada, carga):
+        Coche.__init__(self, color, ruedas, velocidad, cilindrada)
+        self.carga = carga
+    def __str__(self):
+        return Coche.__str__(self) + ", {} kg".format(self.carga)
---
+++ automoviles/motocicleta.py
@@ -0,0 +1,9 @@
+from bicicleta import Bicicleta
+
+class Motocicleta(Bicicleta):
+    def __init__(self, color, ruedas, tipo, velocidad, cilindrada):
+        Bicicleta.__init__(self, color, ruedas, tipo)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+    def __str__(self):
+        return Bicicleta.__str__(self) + ", {} km/h, {}cc".format(self.velocidad, self.cilindrada)
